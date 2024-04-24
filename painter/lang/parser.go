10c46e1ffd0ced6f871a27eb1daeff226e4cc171
SharpDevOps10 <td090833@gmail.com>
2024-04-24 19:47:15 +0300

    Fix problems with coords; Get rid of switch

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -3,6 +3,7 @@ package lang
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"image"
 	"io"
 	"strconv"
@@ -14,6 +15,20 @@ import (
 // Parser уміє прочитати дані з вхідного io.Reader та повернути список операцій представлені вхідним скриптом.
 type Parser struct {
 	painter painter.StatePainter
+}
+
+// CommandFunc представляє функцію, яка обробляє команду.
+type CommandFunc func(p *Parser, args []string) error
+
+// CommandMap містить відображення команд на функції, які їх обробляють.
+var CommandMap = map[string]CommandFunc{
+	"white":  handleWhite,
+	"green":  handleGreen,
+	"update": handleUpdate,
+	"bgrect": handleBgRect,
+	"figure": handleFigure,
+	"move":   handleMove,
+	"reset":  handleReset,
 }
 
 // Parse парсить вхідний потік даних та повертає список операцій.
@@ -40,75 +55,113 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 func (p *Parser) parseCommand(commandLine string) error {
 	fields := strings.Fields(commandLine)
 	if len(fields) == 0 {
-		_ = errors.New("empty command")
+		return errors.New("empty command")
 	}
 
 	command := fields[0]
 	args := fields[1:]
 
-	switch command {
-	case "white":
-		p.painter.SetWhiteBg()
-	case "green":
-		p.painter.SetGreenBg()
-	case "update":
-		p.painter.Update()
-	case "bgrect":
-		if len(args) != 4 {
-			return errors.New("invalid number of arguments for bgrect command")
-		}
-		x1, err := strconv.Atoi(args[0])
-		if err != nil {
-			return err
-		}
-		y1, err := strconv.Atoi(args[1])
-		if err != nil {
-			return err
-		}
-		x2, err := strconv.Atoi(args[2])
-		if err != nil {
-			return err
-		}
-		y2, err := strconv.Atoi(args[3])
-		if err != nil {
-			return err
-		}
-		p.painter.SetBgRectangle(image.Point{X: x1, Y: y1}, image.Point{X: x2, Y: y2})
-		return nil
-	case "figure":
-		if len(args) != 2 {
-			return errors.New("invalid number of arguments for figure command")
-		}
-		x, err := strconv.ParseFloat(args[0], 64)
-		if err != nil {
-			return err
-		}
-		y, err := strconv.ParseFloat(args[1], 64)
-		if err != nil {
-			return err
-		}
-		p.painter.DrawTFigure(image.Point{X: int(x), Y: int(y)})
-		return nil
-	case "move":
-		if len(args) != 2 {
-			return errors.New("invalid number of arguments for move command")
-		}
-		x, err := strconv.Atoi(args[0])
-		if err != nil {
-			return err
-		}
-		y, err := strconv.Atoi(args[1])
-		if err != nil {
-			return err
-		}
-		p.painter.MoveTFigures(x, y)
-		return nil
-	case "reset":
-		p.painter.ResetPainterState()
-		p.painter.ResetBg()
-		return nil
-	default:
-		return errors.New("unknown command: " + command)
+	if cmdFunc, ok := CommandMap[command]; ok {
+		return cmdFunc(p, args)
 	}
+
+	return errors.New("unknown command: " + command)
+}
+
+func handleWhite(p *Parser, args []string) error {
+	if len(args) != 0 {
+		return errors.New("white command requires 0 arguments")
+	}
+	p.painter.SetWhiteBg()
 	return nil
+}
+
+func handleGreen(p *Parser, args []string) error {
+	if len(args) != 0 {
+		return errors.New("green command requires 0 arguments")
+	}
+	p.painter.SetGreenBg()
+	return nil
+}
+
+func handleUpdate(p *Parser, args []string) error {
+	if len(args) != 0 {
+		return errors.New("update command requires 0 arguments")
+	}
+	p.painter.Update()
+	return nil
+}
+
+func handleBgRect(p *Parser, args []string) error {
+	if len(args) != 4 {
+		return errors.New("invalid number of arguments for bgrect command")
+	}
+	x1, err := convertToAbsoluteCoords(args[0])
+	if err != nil {
+		return err
+	}
+	y1, err := convertToAbsoluteCoords(args[1])
+	if err != nil {
+		return err
+	}
+	x2, err := convertToAbsoluteCoords(args[2])
+	if err != nil {
+		return err
+	}
+	y2, err := convertToAbsoluteCoords(args[3])
+	if err != nil {
+		return err
+	}
+	p.painter.SetBgRectangle(image.Point{X: x1, Y: y1}, image.Point{X: x2, Y: y2})
+	return nil
+}
+
+func handleFigure(p *Parser, args []string) error {
+	if len(args) != 2 {
+		return errors.New("invalid number of arguments for figure command")
+	}
+	x, err := convertToAbsoluteCoords(args[0])
+	if err != nil {
+		return err
+	}
+	y, err := convertToAbsoluteCoords(args[1])
+	if err != nil {
+		return err
+	}
+	p.painter.DrawTFigure(image.Point{X: x, Y: y})
+	return nil
+}
+
+func handleMove(p *Parser, args []string) error {
+	if len(args) != 2 {
+		return errors.New("invalid number of arguments for move command")
+	}
+	x, err := convertToAbsoluteCoords(args[0])
+	if err != nil {
+		return err
+	}
+	y, err := convertToAbsoluteCoords(args[1])
+	if err != nil {
+		return err
+	}
+	p.painter.MoveTFigures(x, y)
+	return nil
+}
+
+func handleReset(p *Parser, args []string) error {
+	if len(args) != 0 {
+		return errors.New("reset command requires 0 arguments")
+	}
+	p.painter.ResetPainterState()
+	p.painter.ResetBg()
+	return nil
+}
+
+func convertToAbsoluteCoords(s string) (int, error) {
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0, fmt.Errorf("cannot convert these coordinates: %s", s)
+	}
+
+	return int(f * 800), nil
 }
