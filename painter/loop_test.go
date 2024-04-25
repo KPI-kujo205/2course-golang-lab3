a4c19cbba79539b9968eba79314502c7e7296c3c
thebladehit <boqdanyarmolka@gmail.com>
2024-04-25 12:36:53 +0300

    Add tests for loop painter

--- painter/loop_test.go
+++ painter/loop_test.go
@@ -1,71 +1,16 @@
 package painter
 
 import (
-	"github.com/stretchr/testify/mock"
 	"image"
 	"image/color"
 	"image/draw"
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
 	"golang.org/x/exp/shiny/screen"
 )
-
-func TestLoop_Post(t *testing.T) {
-	var (
-		l  Loop
-		tr testReceiver
-	)
-	l.Receiver = &tr
-
-	var testOps []string
-
-	l.Start(mockScreen{})
-	l.Post(logOp(t, "do white fill", WhiteFill))
-	l.Post(logOp(t, "do green fill", GreenFill))
-	l.Post(UpdateOp)
-
-	for i := 0; i < 3; i++ {
-		go l.Post(logOp(t, "do green fill", GreenFill))
-	}
-
-	l.Post(OperationFunc(func(screen.Texture) {
-		testOps = append(testOps, "op 1")
-		l.Post(OperationFunc(func(screen.Texture) {
-			testOps = append(testOps, "op 2")
-		}))
-	}))
-	l.Post(OperationFunc(func(screen.Texture) {
-		testOps = append(testOps, "op 3")
-	}))
-
-	l.StopAndWait()
-
-	if tr.lastTexture == nil {
-		t.Fatal("Texture was not updated")
-	}
-	mt, ok := tr.lastTexture.(*mockTexture)
-	if !ok {
-		t.Fatal("Unexpected texture", tr.lastTexture)
-	}
-	if mt.Colors[0] != color.White {
-		t.Error("First color is not white:", mt.Colors)
-	}
-	if len(mt.Colors) != 2 {
-		t.Error("Unexpected size of colors:", mt.Colors)
-	}
-
-	if !reflect.DeepEqual(testOps, []string{"op 1", "op 2", "op 3"}) {
-		t.Error("Bad order:", testOps)
-	}
-}
-
-func logOp(t *testing.T, msg string, op OperationFunc) OperationFunc {
-	return func(tx screen.Texture) {
-		t.Log(msg)
-		op(tx)
-	}
-}
 
 type testReceiver struct {
 	lastTexture screen.Texture
@@ -113,4 +58,39 @@ type Mock struct {
 func (mockOperation *Mock) Do(t screen.Texture) bool {
 	args := mockOperation.Called(t)
 	return args.Bool(0)
+}
+
+func TestLoop_Start(t *testing.T) {
+	mockScreen := mockScreen{}
+
+	var l Loop
+	l.Receiver = &testReceiver{}
+	l.Start(mockScreen)
+
+	assert.NotNil(t, l.next)
+	assert.NotNil(t, l.prev)
+}
+
+func TestLoop_Post(t *testing.T) {
+	mockScreen := mockScreen{}
+
+	var l Loop
+	l.Receiver = &testReceiver{}
+	l.Start(mockScreen)
+
+	l.Post(OperationFunc(func(tx screen.Texture) {}))
+	l.Post(OperationFunc(func(tx screen.Texture) {}))
+
+	assert.False(t, l.mq.Empty())
+}
+
+func TestLoop_StopAndWait(t *testing.T) {
+	mockScreen := mockScreen{}
+
+	var l Loop
+	l.Receiver = &testReceiver{}
+	l.Start(mockScreen)
+
+	l.StopAndWait()
+	assert.True(t, l.mq.Empty())
 }
